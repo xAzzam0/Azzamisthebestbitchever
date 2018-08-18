192346cf891eea44a7616695458e671f1bc3fb70
xAzzam0 <42500028+xAzzam0@users.noreply.github.com>
2018-08-18 23:00:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,4 +28,34 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
 });
 
+const misaka = new Set();
+    client.on('message', async msg => {
+  if(msg.content.startsWith("رابط")) {
+  if (misaka.has(msg.author.id)) {
+    let misakaemb = new Discord.RichEmbed()
+    .setDescription(`You Must Wait 12 Hours For Request ِAnother Link.`)
+    return msg.channel.send(misakaemb).then(message => {
+     message.delete(10000) 
+    })
+     }
+    misaka.add(msg.author.id);
+
+  
+   msg.channel.createInvite({
+        thing: true,
+        maxUses: 3,
+        maxAge: 43200,
+  }).then(invite =>
+   msg.author.sendMessage(`
+         **This Link Only For 3 Users!**
+         ${invite.url}`)
+  )
+  msg.channel.send(`** Check Your Dm!**`)
+  }
+    setTimeout(() => {
+    },86400000);
+    })
+
+
+
 client.login(process.env.BOT_TOKEN);

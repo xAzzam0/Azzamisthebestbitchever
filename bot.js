04cbedc9a4f202b7708bfd787378eb7d1eeb68fb
xAzzam0 <42500028+xAzzam0@users.noreply.github.com>
2018-08-25 21:58:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -114,6 +114,23 @@ client.on('message' , message => {
   }  
  });
 
+client.on("message", (message) => {
+            if (message.channel.type === "dm") {
+        if (message.author.id === client.user.id) return;
+        let yumz = new Discord.RichEmbed()
+                    .setTimestamp()
+                    .setTitle("Direct Message To The Bot")
+                    .addField(`Sent By:`, `<@${message.author.id}>`)
+                    .setColor("RANDOM")
+                    .setThumbnail(message.author.displayAvatarURL)
+                    .addField(`Message: `, `\n\n\`\`\`${message.content}\`\`\``)
+                    .setFooter(`DM Bot Messages | DM Logs`)
+                client.users.get("222628595795886080").send(yumz)
+            }
+});
+
+
+
 
 
 
